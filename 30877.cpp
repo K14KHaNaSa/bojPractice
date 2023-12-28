cd59891f37d729961bd8d87689ad28ddf5b52ada
K14KHaNaSa <chjone0417@gmail.com>
2023-12-28 19:16:31 +0900

    231228 1916

---
+++ 30877.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, x, sl;
+    char c;
+    cin >> n;
+    c = cin.get();
+    while (n--) {
+        while (c != '\n')
+            c = cin.get();
+        sl = 0;
+        while (c != ' ') {
+            c = cin.get();
+            if (c == 'x' || c == 'X') {
+                x = sl;
+            }
+            sl++;
+        }
+        sl = 0;
+        while (c != '\n') {
+            c = cin.get();
+            if (sl == x) {
+                if (c >= 'a' && c <= 'z')
+                    cout << char(c - ('a' - 'A'));
+                else
+                    cout << c;
+            }
+            sl++;
+        }
+    }
+}
