4bbba685963abb64ac8c60fe47b11230ae3f55bb
K14KHaNaSa <chjone0417@gmail.com>
2024-01-12 02:21:34 +0900

    240112 0221

--- 2169.cpp
+++ 2169.cpp
@@ -1,45 +1,45 @@
 #include <iostream>
 using namespace std;
 
-int n, m, i, j, k;
-int f[1001][1001]; // floor dp
-int now_value[1001];
-int left_dp[1001]; // ( l -> r)
-int right_dp[1002]; // ( r -> l)
+int n, m, i, j, k, lft;
+int f[1000]; // floor dp
+int now_value[1000];
+int dp[1000]; // ( l -> r)
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
-	cin >> n >> m >> f[0][1];
-	left_dp[0] = 0;
-	right_dp[m + 1] = 0;
-	for (j= 2; j <= m; j++) { // top
-		cin >> f[0][j];
-		f[0][j] += f[0][j - 1];
+	cin >> n >> m >> f[0];
+	for (j= 1; j < m; j++) { // top
+		cin >> f[j];
+		f[j] += f[j - 1];
 	}
 	for (i = 1; i < n - 1; i++) { // mid
-		cin >> now_value[1];
-		left_dp[1] = now_value[1] + f[i - 1][1];
-		for (j = 2; j <= m; j++) { // l -> r
+		cin >> now_value[0];
+		dp[0] = now_value[0] + f[0];
+		for (j = 1; j < m; j++) { // l -> r / left dp
 			cin >> now_value[j];
-			left_dp[j] = max(left_dp[j - 1], f[i - 1][j]) + now_value[j];
+			dp[j] = max(dp[j - 1], f[j]) + now_value[j];
 		}
-		right_dp[m] = now_value[m] + f[i - 1][m];
-		f[i][m] = max(left_dp[m], right_dp[m]);
-		for (j = m - 1; j > 0; j--) { // r -> l
-			right_dp[j] = max(right_dp[j + 1], f[i-1][j]) + now_value[j];
-			f[i][j] = max(left_dp[j], right_dp[j]);
+		int right = dp[m - 1]; // temp func f[j + 1]
+		dp[m - 1] = f[m - 1] + now_value[m - 1]; // right dp
+		for (j = m - 2; j >= 0; j--) { // r -> l
+			lft = dp[j]; // temp func left dp
+			dp[j] = max(f[j], dp[j + 1]) + now_value[j];
+			f[j + 1] = right;
+			right = max(lft, dp[j]);
 		}
+		f[0] = dp[0];
 	}
 	if (n > 1) {
-		cin >> now_value[1];
-		f[n - 1][1] = f[n - 2][1] + now_value[1];
-		for (j = 2; j <= m; j++) { // bottom
+		cin >> now_value[0];
+		f[0] = f[0] + now_value[0];
+		for (j = 1; j < m; j++) { // bottom
 			cin >> now_value[j];
-			f[n - 1][j] = max(f[n - 1][j - 1], f[n - 2][j]) + now_value[j];
+			f[j] = max(f[j - 1], f[j]) + now_value[j];
 		}
 	}
-	cout << f[n - 1][m];
+	cout << f[m - 1];
 }
