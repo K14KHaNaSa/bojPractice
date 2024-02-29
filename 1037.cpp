509b2ed63535dbdc98b48e21a5d55c42bfb9c770
K14KHaNaSa <chjone0417@gmail.com>
2024-02-29 11:14:23 +0900

    240229 1114

---
+++ 1037.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, y;
+	cin >> n;
+	int up = 0;
+	int down = 1000001;
+	while (n--) {
+		cin >> y;
+		up = max(up, y);
+		down = min(down, y);
+	}
+	cout << up * down;
+}
