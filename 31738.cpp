04a3cf484d53f01a693d0c5d7d6f650345fb0b6f
K14KHaNaSa <chjone0417@gmail.com>
2024-04-12 16:24:07 +0900

    240412 1624

---
+++ 31738.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	unsigned long long int n, m, ans;
+	cin >> n >> m;
+	if (n < m || n % m>0) {
+		ans = 1;
+		while (n > 1) {
+			if (n % m == 0) {
+				cout << 0;
+				return 0;
+			}
+			ans = ((n % m) * ans) % m;
+			n--;
+		}
+		cout << ans;
+	}
+	else
+		cout << 0;
+}
