c1ef666225e216975676c55578e9a3ee638d9c8b
K14KHaNaSa <chjone0417@gmail.com>
2024-05-09 22:06:24 +0900

    240509 2206

--- 17094.cpp
+++ 17094.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 using namespace std;
 
+int m[1000001][2];
+
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
-	int n, t, e;
-	char s;
-	cin >> n;
-	s = cin.get();
-	t = 0;
-	e = 0;
-	while (n--) {
-		s = cin.get();
-		if (s == '2')
-			t++;
-		if (s == 'e')
-			e++;
+	int a, b;
+	cin >> a >> b;
+	for (int i = a; i <= b; i++) {
+		m[i][0] = 999999999;
+		m[i][1] = 999999999;
 	}
-	t == e ? cout << "yee" : (t > e ? cout << "2" : cout << "e");
+	m[a][0] = 0;
+	for (int i = a; i < b; i++) {
+		if (m[i][0] < 1000001) {
+			m[i + 1][0] = min(m[i + 1][0], m[i][0] + 1);
+			if (i * 2 <= b)
+				m[i * 2][0] = min(m[i * 2][0], m[i][0] + 1);
+			if (i * 10 <= b)
+				m[i * 10][1] = min(m[i * 10][1], m[i][0] + 1);
+		}
+		if (m[i][1] < 1000001) {
+			m[i + 1][1] = min(m[i + 1][1], m[i][1] + 1);
+			if (i * 2 <= b)
+				m[i * 2][1] = min(m[i * 2][1], m[i][1] + 1);
+		}
+	}
+	cout << min(m[b][0], m[b][1]);
 }
