633653a89a1d70dd43cd043d8e69754734fc8531
K14KHaNaSa <chjone0417@gmail.com>
2024-06-20 15:10:06 +0900

    240620 1510

--- 2491.cpp
+++ 2491.cpp
@@ -1,35 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int a[100000];
-
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
-	int n, i;
+	int n, pre, a;
 	cin >> n;
-	int now = 1;
+	int inc = 1;
+	int dec = 1;
 	int ans = 1;
-	cin >> a[0];
-	for (i = 1; i < n; i++) {
-		cin >> a[i];
-		if (a[i] >= a[i - 1]) {
-			now++;
-			ans = max(ans, now);
-		}
+	cin >> pre;
+	n--;
+	while (n--) {
+		cin >> a;
+		if (a >= pre)
+			inc++;
 		else
-			now = 1;
-	}
-	now = 1;
-	for (i = n - 2; i >= 0; i--) {
-		if (a[i] >= a[i + 1]) {
-			now++;
-			ans = max(ans, now);
-		}
+			inc = 1;
+		if (a <= pre)
+			dec++;
 		else
-			now = 1;
+			dec = 1;
+		ans = max(ans, max(inc, dec));
+		pre = a;
 	}
 	cout << ans;
 }
