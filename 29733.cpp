a720b2f076a673e340cc5d70b4c609603e99bfec
K14KHaNaSa <chjone0417@gmail.com>
2024-08-13 17:22:31 +0900

    240813 1722

--- 29733.cpp
+++ 29733.cpp
@@ -6,34 +6,38 @@ int main(void) {
 	cin.tie(0);
 	cout.tie(0);
 
-	bool q[100][100][100];
+	int q[100][100][100];
 	int r, c, h, i, j, k, x, y, z;
 	char ch;
 	cin >> r >> c >> h;
 	for (k = 0; k < h; k++) {
 		for (i = 0; i < r; i++) {
-			ch = cin.get(); // \n
-			for (j = 0; j< c; j++) {
-				ch = cin.get();
-				(ch == '*') ? q[i][j][k] = true : q[i][j][k] = false;
+			for (j = 0; j < c; j++) {
+				q[i][j][k] = 0;
 			}
 		}
 	}
 	for (k = 0; k < h; k++) {
 		for (i = 0; i < r; i++) {
+			ch = cin.get(); // \n
 			for (j = 0; j < c; j++) {
-				int mine = 0;
-				for (z = max(0, k - 1); z < min(h, k + 2); z++) {
-					for (x = max(0, i - 1); x < min(r, i + 2); x++) {
-						for (y = max(0, j - 1); y < min(c, j + 2); y++) {
-							if (x == r && y == c && z == h) continue;
-							if (q[x][y][z])
-								mine++;
+				ch = cin.get();
+				if (ch == '*') {
+					for (z = max(0, k - 1); z < min(h, k + 2); z++) {
+						for (x = max(0, i - 1); x < min(r, i + 2); x++) {
+							for (y = max(0, j - 1); y < min(c, j + 2); y++)
+								q[x][y][z]++;
 						}
 					}
+					q[i][j][k] = 10000001; // it's == *
 				}
-				q[i][j][k] ? cout << '*' : cout << mine % 10;
 			}
+		}
+	}
+	for (k = 0; k < h; k++) {
+		for (i = 0; i < r; i++) {
+			for (j = 0; j < c; j++)
+				q[i][j][k] > 10000000 ? cout << '*' : cout << q[i][j][k] % 10;
 			cout << '\n';
 		}
 	}
