16b999ee2cd74e457288783de050f71e350b7e7b
K14KHaNaSa <chjone0417@gmail.com>
2024-08-25 18:04:03 +0900

    240825 1804

--- 32143.cpp
+++ 32143.cpp
@@ -2,20 +2,11 @@
 #include <queue>
 using namespace std;
 
-int main(void) {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+priority_queue<long long int> card; // reversed
+int n, q;
+long long int h, d, x, cardsum;
 
-	priority_queue<long long int> card; // reversed
-	long long int cardsum = 0;
-	long long int h, n, q, d, x;
-	cin >> h >> n >> q;
-	while (n--) {
-		cin >> d;
-		cardsum += d;
-		card.push(d * -1);
-	}
+void battle() {
 	if (cardsum < h)
 		cout << -1 << '\n';
 	else {
@@ -25,18 +16,25 @@ int main(void) {
 		}
 		cout << card.size() << '\n';
 	}
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> h >> n >> q;
+	cardsum = 0;
+	while (n--) {
+		cin >> d;
+		cardsum += d;
+		card.push(d * -1);
+	}
+	battle();
 	while (q--) {
 		cin >> x;
 		cardsum += x;
 		card.push(x * -1);
-		if (cardsum < h)
-			cout << -1 << '\n';
-		else {
-			while (cardsum + card.top() >= h) {
-				cardsum += card.top();
-				card.pop();
-			}
-			cout << card.size() << '\n';
-		}
+		battle();
 	}
 }
