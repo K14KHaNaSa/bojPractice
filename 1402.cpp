943734b1f9881bb911c51f00aad41f6845bd0238
K14KHaNaSa <chjone0417@gmail.com>
2024-12-05 18:20:38 +0900

    241205 1820

---
+++ 1402.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int t, a, b;
+	cin >> t;
+	while (t--) {
+		cin >> a >> b;
+		cout << "yes\n";
+	}
+}
