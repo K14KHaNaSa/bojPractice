0fc4f5155a579c66c75eeb1ebb9ca766acc894b3
K14KHaNaSa <chjone0417@gmail.com>
2025-02-05 23:32:05 +0900

    250205 2332

---
+++ 2965.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << max(b - a - 1, c - b - 1);
+}
