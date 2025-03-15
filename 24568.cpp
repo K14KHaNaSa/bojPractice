ba9e10a67114d0c1a29c411bda5ffa6b72b34e66
K14KHaNaSa <chjone0417@gmail.com>
2025-03-15 17:02:01 +0900

    250315 1701

---
+++ 24568.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int r, s;
+    cin >> r >> s;
+    cout << (r * 8 + s * 3) - 28;
+}
