15c49f1837814c5750047b6b394c639be4a6733c
K14KHaNaSa <chjone0417@gmail.com>
2025-06-25 23:27:21 +0900

    250625 2327 remember 0625

--- 32604.cpp
+++ 32604.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 using namespace std;
 
 int main(void) {
@@ -6,18 +8,9 @@ int main(void) {
     cin.tie(0);
     cout.tie(0);
 
-    int n, i;
-    int ab[100][2];
-    bool ans = true;
-    cin >> n;
-    for (i = 0; i < n; i++)
-        cin >> ab[i][0] >> ab[i][1];
-    for (i = 1; i < n; i++) {
-        if (ab[i - 1][0] <= ab[i][0] && ab[i - 1][1] <= ab[i][1]) continue;
-        else {
-            ans = false;
-            break;
-        }
-    }
-    ans ? cout << "yes" : cout << "no";
+    double a;
+    cin >> a;
+    double pi = 3.14159265359;
+    a /= pi;
+    cout << fixed << setprecision(15) << sqrt(a) * 2 * pi;
 }
