80d68bc0e26b3fcc7b152628fb0977d5873818c5
Jaewon Chung <chjone0417@gmail.com>
2025-11-15 02:28:36 +0900

    251115 0228

---
+++ 34724.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    char x[1000][1000];
+    int n, m;
+    char c;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        c = cin.get();
+        for (int j = 0; j < m; j++) {
+            c = cin.get();
+            x[i][j] = c;
+        }
+    }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < m - 1; j++) {
+            if (x[i][j] == '1') {
+                if (x[i + 1][j] == '1' && x[i][j + 1] == '1' && x[i + 1][j + 1] == '1') {
+                    cout << 1;
+                    return 0;
+                }
+            }
+        }
+    }
+    cout << 0;
+    return 0;
+}
